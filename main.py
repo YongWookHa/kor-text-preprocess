88059ce4ba8227616127419c45f8b24c605b5532
YongWook Ha <ywha12@gmail.com>
2019-06-11 10:25:53 +0900

    add: Byte Pair Encoder of sentencepiece

--- main.py
+++ main.py
@@ -15,19 +15,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-i", "--input", \
-                        required=True, type=str, help="Input file name")
+                        required=True, type=str, help="Input file path")
     parser.add_argument("-o", "--output", \
-                        type=str, default="ouput.txt", help="Output file name")
+                        type=str, default="ouput.txt", help="Output file path")
     parser.add_argument("-opt", "--option", \
                         required=True, type=str, default=None, \
                         choices=opt_availables, help="Which option to apply.")
-    parser.add_argument("--mecab", action="store_true", help="Use mecab for tokenizing")
+    parser.add_argument("--mecab", action="store_true", help="Use mecab tokenizer")
     parser.add_argument("--morphs", action="store_true", \
                         help="Apply \'morphs\' of the tokenizer to the text")
     parser.add_argument("--nouns", action="store_true", \
                         help="Apply \'nouns\' of the tokenizer to the text")
     parser.add_argument("--pos", action="store_true", \
                         help="Apply \'pos\' of the tokenizer to the text")
+    parser.add_argument("--bpe", action="store_true", help="Use BPE tokenizer")
+    parser.add_argument("--train", action="store_true", help="Train BPE tokenizer")
+    parser.add_argument("-v", "--vocab", type=int, default=100000, help="BPE vocabulary size")
+    parser.add_argument("--model", type=str, default=None, help="Pre-trained BPE model path")
     parser.add_argument("--nsp", action="store_true", help="Which task to apply.")
     parser.add_argument("-ms", "--min_seq", type=int, default=0, \
                         help="Minimum length of sequence")
--- src/tokenize.py
+++ src/tokenize.py
@@ -1,7 +1,8 @@
 from .utils import Refine
 from tqdm import tqdm
 from konlpy.tag import Mecab
-from os.path import normpath
+from os.path import normpath, basename
+import sentencepiece as spm
 
 class Tokenizer(Refine):
     def __init__(self, args):
@@ -22,6 +23,10 @@ class Tokenizer(Refine):
             else:
                 print("found no method in [morphs, nouns, pos]")
                 raise ValueError
+        elif args.bpe:
+            self.cls = self.bpe
+            self.args = [normpath(args.input), normpath(args.output), \
+                        args.model, args.train, args.vocab, args.encoding]
         else:
             print("Tokenizer class indicate error: {} not available"\
                 .format())
@@ -51,3 +56,34 @@ class Tokenizer(Refine):
                         out.write(applied[-1] + '\n')
                         
         print("process done, saved at {}".format(out_path))
+
+    def bpe(self, inp_path, out_path, model: str, train: bool, \
+                                    vocab: int, encoding='utf8'):
+        if train:
+            templates = '--input={} --model_prefix={} --vocab_size={}'
+            vocab_size = vocab
+            prefix = basename(out_path)
+            cmd = templates.format(inp_path, prefix, vocab_size)
+
+            spm.SentencePieceTrainer.Train(cmd)
+
+        else:
+            assert model and "BPE model path: None"
+
+            sp = spm.SentencePieceProcessor()
+            sp.Load(normpath(model))
+            num_lines = sum(1 for line in open(inp_path, "r", encoding=encoding))
+            lines = super().readline(inp_path, encoding)
+            with open(out_path, "w", encoding=encoding) as out:
+                for line in tqdm(lines, total=num_lines):
+                    if not line:
+                        break
+                    bpe = sp.EncodeAsPieces(line)
+                    for x in bpe[:-1]:
+                        out.write(x+' ')
+                    try:
+                        out.write(bpe[-1]+'\n')
+                    except IndexError:
+                        out.write('\n')
+                            
+            print("process done, saved at {}".format(out_path))
